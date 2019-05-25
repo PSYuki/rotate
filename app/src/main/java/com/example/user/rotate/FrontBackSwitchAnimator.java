b22540d7c07d54bf34910f2627b22e3e6cf11342
koheinishizawa <sasaran133-kisslingo@yahoo.co.jp>
2019-05-25 23:17:33 +0900

    rotate

---
+++ app/src/main/java/com/example/user/rotate/FrontBackSwitchAnimator.java
@@ -0,0 +1,81 @@
+package com.example.user.rotate;
+
+import android.graphics.Camera;
+import android.graphics.Matrix;
+import android.view.View;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.view.animation.Animation;
+import android.view.animation.Transformation;
+
+public class FrontBackSwitchAnimator extends Animation {
+
+    private Camera camera;
+
+    /** 表面のView*/
+    private View frontView;
+
+    /** 裏面のView*/
+    private View BackView;
+
+    private float centerX;
+
+    private float centerY;
+
+    private boolean forward = true;
+
+    private boolean visibilitySwapped;
+
+    public FrontBackSwitchAnimator(View frontView, View BackView, int centerX, int centerY) {
+        this.frontView = frontView;
+        this.BackView = BackView;
+        this.centerX = centerX;
+        this.centerY = centerY;
+
+        setDuration(500);
+        setFillAfter(true);
+        setInterpolator(new AccelerateDecelerateInterpolator());
+    }
+
+    public void reverse() {
+        forward = false;
+        View temp = BackView;
+        BackView = frontView;
+        frontView = temp;
+    }
+
+    @Override
+    public void initialize(int width, int height, int parentWidth, int parentHeight) {
+        super.initialize(width, height, parentWidth, parentHeight);
+        camera = new Camera();
+    }
+
+    @Override
+    protected void applyTransformation(float interpolatedTime, Transformation t) {
+        final double radians = Math.PI * interpolatedTime;
+        float degrees = (float) (180.0 * radians / Math.PI);
+
+        if (interpolatedTime >= 0.5f) {
+            degrees -= 180.f;
+
+            if (!visibilitySwapped) {
+                frontView.setVisibility(View.GONE);
+                BackView.setVisibility(View.VISIBLE);
+                visibilitySwapped = true;
+            }
+        }
+
+        if (!forward)
+            degrees = -degrees;
+
+        final Matrix matrix = t.getMatrix();
+
+        camera.save();
+        camera.translate(0.0f, 0.0f, (float) (150.0 * Math.sin(radians)));
+        camera.rotateX(degrees); // ここをrotateX()にすると、縦方向に回転します
+        camera.getMatrix(matrix);
+        camera.restore();
+
+        matrix.preTranslate(-centerX, -centerY);
+        matrix.postTranslate(centerX, centerY);
+    }
+}
---
+++ app/src/main/java/com/example/user/rotate/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.user.rotate;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+/*public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}*/
+
+
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+
+public class MainActivity extends AppCompatActivity implements OnClickListener {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.front_back);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switchFrontBackView(false);  // 表から裏にひっくり返す場合
+//		switchFrontBackView(true);   // 裏から表にひっくり返す場合
+    }
+
+    private void switchFrontBackView(boolean isReverse) {
+        RelativeLayout relativeLayout = (RelativeLayout) findViewById(R.id.layout);
+        View frontView = (ImageView)findViewById(R.id.imageViewFront);
+        View backView = (ImageView)findViewById(R.id.imageViewBack);
+
+        if(frontView == null || backView == null) return;
+
+        FrontBackSwitchAnimator animator =
+                new FrontBackSwitchAnimator(frontView, backView, backView.getWidth() /2 , backView.getHeight() / 2);
+        if(isReverse) animator.reverse();
+        relativeLayout.startAnimation(animator);
+    }
+}
